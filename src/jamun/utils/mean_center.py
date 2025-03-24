790ddfc2eb4a8d7d67f9a3dbd1f133b60cce5c48
Joseph Kleinhenz <kleinhej@gene.com>
2025-03-24 17:13:51 -0400

    avoid inplace modification

--- src/jamun/utils/mean_center.py
+++ src/jamun/utils/mean_center.py
@@ -6,5 +6,5 @@ def mean_center(x: torch_geometric.data.Batch) -> torch_geometric.data.Batch:
     """Mean centers the positions."""
     x = x.clone("pos")
     mean_pos = scatter(x.pos, x.batch, dim=0, dim_size=x.num_graphs, reduce="mean")
-    x.pos -= mean_pos[x.batch]
+    x.pos = x.pos - mean_pos[x.batch]
     return x
