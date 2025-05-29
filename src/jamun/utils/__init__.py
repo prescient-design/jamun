a170eb5de7d453d848a1070db78e1fc9e672c9ad
Joseph Kleinhenz <kleinhej@gene.com>
2025-05-29 01:23:18 +0000

    add simple ddp
    compatible with torch.compile for energy model

--- src/jamun/utils/__init__.py
+++ src/jamun/utils/__init__.py
@@ -1,8 +1,16 @@
 from .align import align_A_to_B, align_A_to_B_batched
 from .average_squared_distance import compute_average_squared_distance, compute_average_squared_distance_from_datasets
-from .checkpoint import find_checkpoint, find_checkpoint_directory, get_wandb_run_config, get_run_path_for_wandb_run
+from .checkpoint import find_checkpoint, find_checkpoint_directory, get_run_path_for_wandb_run, get_wandb_run_config
 from .data_with_residue_info import DataWithResidueInformation
 from .dist_log import dist_log, wandb_dist_log
+from .featurize_macrocycles import (
+    featurize_macrocycle_atoms,
+    featurize_macrocycle_atoms_from_file,
+    get_amino_acid_stereo,
+    get_residues,
+    get_side_chain_torsion_idxs,
+    one_k_encoding,
+)
 from .mdtraj import coordinates_to_trajectories, save_pdb
 from .mean_center import mean_center
 from .plot import animate_trajectory_with_py3Dmol, plot_molecules_with_py3Dmol
@@ -19,7 +27,7 @@ from .residue_metadata import (
 )
 from .sampling_wrapper import ModelSamplingWrapper
 from .scaled_rmsd import scaled_rmsd
-from .unsqueeze_trailing import unsqueeze_trailing
-from .slurm import wait_for_jobs
-from .featurize_macrocycles import one_k_encoding,featurize_macrocycle_atoms,featurize_macrocycle_atoms_from_file,get_amino_acid_stereo,get_residues,get_side_chain_torsion_idxs
+from .simple_ddp import SimpleDDPStrategy
 from .singleton import singleton
+from .slurm import wait_for_jobs
+from .unsqueeze_trailing import unsqueeze_trailing
