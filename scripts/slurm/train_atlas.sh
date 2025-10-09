47bc4ee68fa59274aafef6c5741e32862dcb48da
Ameya Daigavane <ameya.d.98@gmail.com>
2025-10-09 17:34:31 +0000

    Latest experiments.

--- scripts/slurm/train_atlas.sh
+++ scripts/slurm/train_atlas.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#SBATCH --partition gpu2
+#SBATCH --partition g6e
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 2
-#SBATCH --gpus-per-node 2
+#SBATCH --ntasks-per-node 1
+#SBATCH --gpus-per-node 1
 #SBATCH --cpus-per-task 8
 #SBATCH --time 3-0
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem=32G
 
 eval "$(conda shell.bash hook)"
 conda activate jamun
@@ -17,6 +17,8 @@ echo "SLURM_JOB_ID = ${SLURM_JOB_ID}"
 echo "hostname = $(hostname)"
 
 export HYDRA_FULL_ERROR=1
+export CUDA_HOME=/usr/local/cuda-12.8/
+
 # export TORCH_COMPILE_DEBUG=1
 # export TORCH_LOGS="+dynamo"
 # export TORCHDYNAMO_VERBOSE=1
@@ -29,8 +31,8 @@ nvidia-smi
 
 srun --cpus-per-task 8 --cpu-bind=cores,verbose \
   jamun_train --config-dir=/homefs/home/daigavaa/jamun/configs \
-    experiment=train_uncapped_4AA.yaml \
+    experiment=train_atlas.yaml \
     ++trainer.devices=$SLURM_GPUS_PER_NODE \
     ++trainer.num_nodes=$SLURM_JOB_NUM_NODES \
-    ++logger.wandb.tags=["'${SLURM_JOB_ID}'","'${RUN_KEY}'","train","uncapped_4AA"] \
+    ++logger.wandb.tags=["'${SLURM_JOB_ID}'","'${RUN_KEY}'","train","atlas"] \
     ++run_key=$RUN_KEY
